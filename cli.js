a22252d7830cf0d5df2fa6537bc0f6d2933c4c0d
Jonathan Kim <jkimbo@gmail.com>
2020-04-23 15:29:52 +0100

    Use status api to get more accurate results

--- cli.js
+++ cli.js
@@ -10,15 +10,25 @@ meow(`
 `);
 
 (async () => {
-	try {
-		await got.head('https://github.com', {
-			timeout: 10000,
-			retry: 2
-		});
+	const result = await got('https://kctbh9vrtdwd.statuspage.io/api/v2/summary.json', {
+		timeout: 10000,
+		retry: 2
+	}).json();
 
-		console.error('\n 🐈  It\'s up. Go back to work!');
+	if (['major', 'critical'].includes(result.status.indicator) === true) {
+		console.log('\n🦄  It\'s down. Play with your 😸/🐶! And stay home!\n');
+		console.log('Status page: https://www.githubstatus.com/');
 		process.exitCode = 1;
-	} catch {
-		console.log('\n🦄  It\'s down. Play with your 😸/🐶! And stay home!');
+		return;
 	}
+
+	if (result.status.indicator === 'minor') {
+		console.log('\n🤔 There might be some issues. Probably better to play with your 😸/🐶 instead! Also stay at home!\n');
+		console.log('Status page: https://www.githubstatus.com/');
+		process.exitCode = 1;
+		return;
+	}
+
+	console.error('\n 🐈  It\'s up. Go back to work!');
+	process.exitCode = 0;
 })();
